2c628cc5a538e32cb8247b363fc2d0fd5f15dacf
Justin Wilkinson <wilkinsense@gmail.com>
2016-11-04 11:13:15 -0400

    Work from Monday's class.

---
+++ Assets/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+
+  float angle = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    angle += 40.0f *Time.deltaTime;
+    transform.localRotation = Quaternion.Euler(0.0f, angle, 0.0f);
+	}
+}
---
+++ Assets/TranslateObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TranslateObject : MonoBehaviour {
+
+  [HideInInspector]
+  public int value;
+
+  public Vector3 direction;
+
+  void Awake() {
+
+  }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame at a variable time interval.
+	void Update () {
+    Transform transformComponent = GetComponent<Transform>();
+
+    transform.position += direction * Time.deltaTime;
+	}
+
+  // Update is called once per frame after all other updates have finished.
+  void LateUpdate() {
+  }
+  
+  // Update is called once per frame at a fixed time interval.
+  void FixedUpdate() {
+  }
+
+  /* OnDestroy is called when this component (and its parent GameObject)
+   * are being destroyed. */
+  void OnDestroy() {
+
+  }
+}
